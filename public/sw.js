8aab17406197d20e27ff93347012ce7fcecedc57
FilipeNonemacher <filipenonemacher29@hotmail.com>
2026-09-15 22:20:33 -0300

    Icone Att

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'recepcao-jantar-v1';
+const CACHE_NAME = 'recepcao-jantar-v2';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.add('./')));
